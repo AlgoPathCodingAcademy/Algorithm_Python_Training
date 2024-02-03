3aa705c76ac4614368b560b6121e272bd02c9cf4
Jianming Qiao <jqiao@code-ai.co.uk>
2024-02-03 14:19:32 +0000

    add pie game exercise solution

---
+++ week2/04-Pygame/Instructor/pie_game_exercise_solution.py
@@ -0,0 +1,52 @@
+import random, sys, copy, os, pygame
+from pygame.locals import *
+
+pygame.init()
+window  = pygame.display.set_mode((600, 400))
+pygame.display.set_caption("Pygame Demo")
+
+
+key_c_pressed = False
+key_l_pressed = False
+
+running = True
+while running:
+
+    window.fill((0, 0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_c:
+              #The center of the circle is at 300,200.
+              #The radius of the circle is 50 and the color of the circle is Green.
+              
+              key_c_pressed = not key_c_pressed
+
+
+            elif event.key == pygame.K_l:
+              key_l_pressed = not key_l_pressed
+              #The start of the line is at 100,100.
+              #The end of the line is at 500, 300.
+              #The color of the line is Blue.
+              #The width of the line is 5
+
+              
+    if (key_c_pressed):
+      # Define circle properties
+      circle_color = (0, 255, 0)  # Green color
+      circle_center = (300, 200)  # Center of the screen
+      circle_radius = 50          # Radius of the circle
+
+      # Draw the circle
+      pygame.draw.circle(window, circle_color, circle_center, circle_radius)
+    if key_l_pressed:
+      line_color = (0, 255, 0)  # Green color
+      start_pos = (100, 100)    # Starting position of the line
+      end_pos = (500, 300)      # Ending position of the line
+      line_width = 5            # Width of the line
+
+      # Draw the line
+      pygame.draw.line(window, line_color, start_pos, end_pos, line_width)
+
+    pygame.display.update()
