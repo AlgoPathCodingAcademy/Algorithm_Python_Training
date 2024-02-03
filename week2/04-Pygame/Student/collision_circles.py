9b5180a0e6a072bcb1dbca412d70b53c9029c90f
Jianming Qiao <jqiao@code-ai.co.uk>
2024-02-03 16:24:09 +0000

    Add some math examples

--- week2/04-Pygame/Student/collision_circles.py
+++ week2/04-Pygame/Student/collision_circles.py
@@ -1,3 +1,17 @@
+import math
+
+# Squaring a number
+number = 4
+squared = number ** 2
+print(f"The square of {number} is {squared}.")  # Output: The square of 4 is 16.
+
+
+# Calculating the square root
+number = 16
+square_root = math.sqrt(number)
+print(f"The square root of {number} is {square_root}.")  # Output: The square root of 16 is 4.0.
+
+
 def check_collision(x1, y1, r1, x2, y2, r2):
     #TODO add your code to detect collision
 
