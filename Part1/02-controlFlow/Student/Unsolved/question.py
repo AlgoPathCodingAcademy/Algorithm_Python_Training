300ccc80b45193f6f4efa5afab7c5e95e5a8b2ab
jianming qiao <jianming.qiao@code-ai.co.uk>
2024-05-06 15:28:08 +0100

    Add some sequence code

--- Part1/02-controlFlow/Student/Unsolved/question.py
+++ Part1/02-controlFlow/Student/Unsolved/question.py
@@ -1,3 +1,11 @@
+#Sequence Structure
+#1. Create a Python program that asks the user for their name and then prints a greeting message.
+#2. Create a Python program that takes two numbers from the user, adds them together, and prints the result.
+#3. Create a Python program that calculates how old the user will be in a certain number of years.
+#4. Create a simple unit converter that asks the user for a measurement in kilometers and converts it to miles.
+#Note 1 mile = 0.621371 kilometers
+
+#Selection Structure
 #TODO Finish the following coding task
 #You are given a variable temperature that represents the current temperature in degrees Celsius by using python function input().
 #Write a Python program prints the state of water at that temperature.
@@ -5,7 +13,7 @@
 #If the temperature is between 0°C (inclusive) and 100°C (exclusive), it should print "The water is in liquid state."
 #If the temperature is 100°C or above, it should print "The water is in gaseous state (steam)."
 
-
+#Iteration Structure
 #TODO Print the even number up to 15 using for loop
 
 
