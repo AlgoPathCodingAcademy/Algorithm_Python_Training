6e3ae0671af9dfa546d1fd6d0b95e60c891c8cb0
Jianming Qiao <jqiao@code-ai.co.uk>
2025-03-15 23:34:13 +0000

    Add student exercise for bfs 0/1 remove walls

---
+++ part3/graph/Student/bfs_0_1_remove_walls.py
@@ -0,0 +1,7 @@
+from collections import deque
+
+def minimumObstacles(grid):
+
+    
+print(minimumObstacles([[0,1,1],[1,1,0],[1,1,0]])) #2
+print(minimumObstacles([[0,1,0,0,0],[0,1,0,1,0],[0,0,0,1,0]])) #0
