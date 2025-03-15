44a4f346cef2ff716c84cde9df7aa30a74cee267
Jianming Qiao <jqiao@code-ai.co.uk>
2025-03-15 23:22:53 +0000

    Add student 0/1 bfs

---
+++ part3/graph/Student/bfs_0_1_basic.py
@@ -0,0 +1,32 @@
+graph = {
+    1: [(2, 0), (4, 1)],
+    2: [(1, 0), (3, 1)],
+    3: [(2, 1), (5, 0)],
+    4: [(1, 1), (5, 1)],
+    5: [(3, 0), (4, 1)]
+}
+
+from collections import deque
+
+start = 1
+to_do_list = deque()
+
+to_do_list.append(start)
+
+path = {}
+
+for node in graph:
+    path[node] = float("inf")
+
+path[start] = 0
+
+while True:
+    if len(to_do_list) == 0:
+        break
+
+    current = to_do_list.popleft()
+
+    neighbors = graph[current]
+
+    # TODO
+print(path)
