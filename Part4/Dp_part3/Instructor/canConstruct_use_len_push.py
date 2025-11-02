75c9519bcf437004b333368f4a783afdf054d029
Jianming Qiao <jqiao@code-ai.co.uk>
2025-11-02 13:06:18 +0000

    Add canConstruct push

---
+++ Part4/Dp_part3/Instructor/canConstruct_use_len_push.py
@@ -0,0 +1,32 @@
+def canConstruct(target, wordBank, dp):
+    for i in range(len(target)):
+        for item in wordBank:
+            if dp[i]:
+                if target[i:i+len(item)] == item:
+                    dp[i+len(item)] = True
+
+    print(dp)
+    return dp[len(target)]
+
+def wordBreak(s, wordDict):
+    if not s:
+        return True
+    dp = [False] * (len(s)+1)
+    dp[0] = True
+    return canConstruct(s,wordDict,dp)
+    
+s = "catsandog"
+wordDict = ["cats","dog","sand","and","cat"]
+
+print(wordBreak(s,wordDict))
+
+s = "leetcode"
+wordDict = ["leet","code"]
+
+print(wordBreak(s,wordDict))
+
+s = "catsandog"
+
+wordDict = ["cats","dog","sand","and","cat"]
+
+print(wordBreak(s,wordDict))
