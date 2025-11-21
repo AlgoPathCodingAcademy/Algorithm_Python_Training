2bdcf6af5b3fbbd640bc4e39ddb15afaaddbb1cc
Jianming Qiao <jqiao@code-ai.co.uk>
2025-11-21 01:11:47 +0000

    Add knapsack_v

---
+++ Part4/Dp_part4/Instructor/knapsack_v.py
@@ -0,0 +1,19 @@
+weights = [1, 2, 3, 3,7]
+capacity = 7
+
+dp = [[0]*(capacity+1) for _ in range(len(weights) + 1)]
+
+for i in range(len(weights) + 1):
+    dp[i][0] = 1
+
+for i in range(1, len(weights) + 1):
+    for w in range(1, capacity+1):
+
+        # The original formular is
+        # dp[i][w] = dp[i-1][w] + dp[i-1][w-weights[i-1]]
+
+        dp[i][w] = dp[i-1][w]
+        if w >= weights[i-1]:
+            dp[i][w] =  dp[i][w] + dp[i-1][w-weights[i-1]]
+            
+print(dp)
