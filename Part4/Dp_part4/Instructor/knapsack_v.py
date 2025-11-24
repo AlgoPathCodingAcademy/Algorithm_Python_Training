d9f6d4a3c4ac253391700f5b1282e7a94f4e35f4
Jianming Qiao <jqiao@code-ai.co.uk>
2025-11-24 22:29:44 +0000

    Change to general formular

--- Part4/Dp_part4/Instructor/knapsack_v.py
+++ Part4/Dp_part4/Instructor/knapsack_v.py
@@ -12,8 +12,10 @@ for i in range(1, len(weights) + 1):
         # The original formular is
         # dp[i][w] = dp[i-1][w] + dp[i-1][w-weights[i-1]]
 
-        dp[i][w] = dp[i-1][w]
+        if w < weights[i-1]:
+            dp[i][w] = dp[i-1][w]
         if w >= weights[i-1]:
-            dp[i][w] =  dp[i][w] + dp[i-1][w-weights[i-1]]
+            dp[i][w] =  dp[i-1][w] + dp[i-1][w-weights[i-1]]
+
             
 print(dp)
