dc61c0e6e014fb06f02ac98b234c6ee4abd2d085
Jianming Qiao <jqiao@code-ai.co.uk>
2025-12-10 15:13:58 +0000

    Remove DP[i] inside max

--- Part4/Dp_part3/Instructor/min_cost_climb.py
+++ Part4/Dp_part3/Instructor/min_cost_climb.py
@@ -5,6 +5,6 @@ dp[0] = 0
 dp[1] = 0
 
 for i in range(2,len(cost) + 1):
-    dp[i] = min(dp[i-1] + cost[i-1],dp[i-2] + cost[i-2],dp[i])
+    dp[i] = min(dp[i-1] + cost[i-1],dp[i-2] + cost[i-2])
 
 print(dp[-1])
