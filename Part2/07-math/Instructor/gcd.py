538a5492c602498ffb4db43e7194999fee6f5d45
Jianming Qiao <jqiao@code-ai.co.uk>
2025-12-11 15:26:39 +0000

    Add funciton calls to gcd

--- Part2/07-math/Instructor/gcd.py
+++ Part2/07-math/Instructor/gcd.py
@@ -12,3 +12,9 @@ def gcd(a,b):
 
 # The output should be 3
 print(gcd(1701,3768))
+
+def gcd_list(nums):
+    g = nums[0]
+    for i in range(1, len(nums)):
+        g = gcd(g, nums[i])
+    return g
