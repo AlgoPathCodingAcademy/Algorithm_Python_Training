b41894d8d71dbb43a6075df0084c7c7bfcbec00d
Jianming Qiao <jqiao@code-ai.co.uk>
2026-01-24 22:59:22 +0000

    Leave only one Entry-marker version

--- part3/topological_sort/Instructor/direct_graph_is_cycle.py
+++ part3/topological_sort/Instructor/direct_graph_is_cycle.py
@@ -1,109 +1,40 @@
-def dfs(node, visited, visiting):
-    # mark entry
-    visiting.add(node)
+adj_list = {
+    0: [1, 3, 5],
+    1: [2, 3],
+    2: [],
+    3: [4],
+    4: [5],
+    5: [3]
+}
 
-    for neighbor in adj_list.get(node, []):
-        # Case 1: back edge → cycle
-        if neighbor in visiting:
+# V1
+def dfs(node, adj_list, visited, path):
+    # entry-marking
+    visited[node] = True
+    path[node] = True
+
+    for neighbor in adj_list[node]:
+        if neighbor not in visited:
+            if dfs(neighbor, adj_list, visited, path):
+                return True
+        elif neighbor in path:
+            # visited + still in path ⇒ cycle
             return True
 
-        # Case 2: tree edge → recurse
-        if neighbor not in visited:
-            if dfs(neighbor, visited, visiting):
-                return True
-
-    # mark exit
-    visiting.remove(node)
-    visited.add(node)
+    # exit-marking
+    del path[node]
     return False
 
 
-visited = set()
-visiting = set()
+visited = {}
+path = {}
 has_cycle = False
 
 for node in adj_list:
     if node not in visited:
-        if dfs(node, visited, visiting):
+        if dfs(node, adj_list, visited, path):
             has_cycle = True
             break
 
 print(has_cycle)
-
-
-adj_list = {
-    0: [1, 3, 5],
-    1: [2, 3],
-    2: [],
-    3: [4],
-    4: [5],
-    5: [3]
-}
-
-def dfs(node,visited,path):
-    if node in path:
-        print("cycle detected", node, "in",path)
-        return True
-    
-    if node in visited:
-        return False
-    
-    visited[node] = True
-    path[node] = True
-    
-    neighbors = adj_list[node]
-    
-    for neighbor in neighbors:
-        if dfs(neighbor,visited,path):
-            return True
-    
-    del path[node]
-        
-    return False
-        
-visited = {}
-path = {}
-for key in adj_list:
-    if key not in visited:
-        if dfs(key,visited,path):
-            break 
-
-
-# v2 verison 
-adj_list = {
-    0: [1, 3, 5],
-    1: [2, 3],
-    2: [],
-    3: [4],
-    4: [5],
-    5: [3]
-}
-
-def dfs_v2(node,path,visited):
-    for neighbor in adj_list[node]:
-        if neighbor in path:
-            print("Cycle Detected", "neighbor",neighbor,"in path",path)
-            return True
-
-        if neighbor not in visited:
-            path[neighbor] = True
-            visited[neighbor] = True
-            if dfs_v2(neighbor,path,visited):
-                return True
-            del path[neighbor]
-
-    return False
-
-
-visited = {}
-path = {}
-for key in adj_list:
-    if key not in visited:
-        visited[key] = True
-        path[key] = True
-        if dfs_v2(key,path,visited):
-            break
-        del path[key]
-
-
 
