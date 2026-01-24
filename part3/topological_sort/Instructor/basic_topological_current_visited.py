6a48937ea4281e8d7a4545226bf668ea4a3da3ba
Jianming Qiao <jqiao@code-ai.co.uk>
2026-01-24 21:42:49 +0000

    Add changes to the topological - current version

--- part3/topological_sort/Instructor/basic_topological_current_visited.py
+++ part3/topological_sort/Instructor/basic_topological_current_visited.py
@@ -8,47 +8,31 @@ for u,v in edges:
         adj_list[u] = [v]
     else:
         adj_list[u].append(v)
-        
 
+# The numbers of node would normally be provided by the question
+# In our test case, it is 6.
+for node in range(6):
+    if node not in adj_list:
+        adj_list[node] = []
+        
 print(adj_list)
 
 
-#adj_list = {
-#    'A': ['C'],
-#    'B': ['C', 'D'],
-#    'C': ['D'],
-#    'D': ['E'],
-#    'E': []
-#}
-
-def getNeighbors(current):
-
-    if current not in adj_list:
-        return []
-        
-    return adj_list[current]
-    
-    
-result = []
-
-def dfs(node, visited):
-    
-    if node in visited:
-        return
-    
-    neighbors = getNeighbors(node)
-    
+def dfs(graph, node,visited,result):
     visited[node] = True
     
-    for neighbor in neighbors:
-        dfs(neighbor,visited)
-
+    for neighbor in graph[node]:
+        if neighbor not in visited:
+            dfs(graph,neighbor,visited,result)
+            
     result.append(node)
-        
-
-visited = {}
-for key in adj_list:
-    dfs(key,visited)
     
+    
+visited = {}
+result = []
+for node in adj_list:
+    if node not in visited:
+        dfs(adj_list, node,visited,result)
+        
 print(result[::-1])
     
